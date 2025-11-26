d419d95ccc59843ba3d5bd494d5039bc3a22933f
Tatsuya Noguchi <52320299+tatsuyano9@users.noreply.github.com>
2025-11-27 01:00:43 +0900

    Issue #39 ドメインの型定義を作成 (#40)

---
+++ src/types/domain/match.ts
@@ -0,0 +1,24 @@
+import { User } from "../domain";
+
+export type Match = {
+  id: string;
+  leagueId: string;
+  playedAt: Date;
+  createdAt: Date;
+  results: MatchResult;
+};
+
+export type Wind = "EAST" | "SOUTH" | "WEST" | "NORTH";
+
+export type IndividualMatchResult = {
+  player: User;
+  score: number;
+  rank: number;
+};
+
+export type MatchResultInput = Record<Wind, IndividualMatchResult>;
+
+export type MatchResult = {
+  matchResultInput: MatchResultInput;
+  createdAt: Date;
+};
